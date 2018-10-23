d993d16e19fc8e8b405f8fe0f49cc931569de419
xilin <xilin@xilindeMacBook-Pro.local>
2018-10-23 17:53:21 -0400

    Added scoring mechanism

---
+++ Android-App/chatbot-watson-android/app/src/main/java/com/example/vmac/WatBot/score/ScoreCalculate.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class ScoreCalculate implements ScoreCalculator {
+
+    @Override
+    public int getScore(ArrayList<String> AllSymptoms,
+            ArrayList<String> AskedSymptoms, ArrayList<String> ResponsedAnswer,
+            String CorrectAnswer) {
+        int score = 0;
+        int countTotal = AllSymptoms.size();
+        int countAsked = AskedSymptoms.size();
+
+        for (int i = 0; i < ResponsedAnswer.size(); i++) {
+            if (ResponsedAnswer.get(i).equals(CorrectAnswer)) {
+                score += 50;
+                int scorePercentage = (countTotal - countAsked) * 100
+                        / countTotal;
+                if (scorePercentage > 90) {
+                    score = 100;
+                } else {
+                    score += scorePercentage / 2;
+                }
+            } else {
+                score -= 10;
+            }
+        }
+        return score;
+    }
+}
---
+++ Android-App/chatbot-watson-android/app/src/main/java/com/example/vmac/WatBot/score/ScoreCalculator.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public interface ScoreCalculator {
+
+    public int getScore(ArrayList<String> AllSymptoms,
+            ArrayList<String> AskedSymptoms, ArrayList<String> ResponsedAnswer,
+            String CorrectAnswer);
+}
