573bcabafd5be78a050ca68dd0656322bea781d4
xilin <xilin@xilindeMacBook-Pro.local>
2018-10-23 18:54:01 -0400

    modify score methods

--- Android-App/chatbot-watson-android/app/src/main/java/com/example/vmac/WatBot/score/ScoreCalculate.java
+++ Android-App/chatbot-watson-android/app/src/main/java/com/example/vmac/WatBot/score/ScoreCalculate.java
@@ -10,20 +10,57 @@ public class ScoreCalculate implements ScoreCalculator {
         int countTotal = AllSymptoms.size();
         int countAsked = AskedSymptoms.size();
 
-        for (int i = 0; i < ResponsedAnswer.size(); i++) {
-            if (ResponsedAnswer.get(i).equals(CorrectAnswer)) {
-                score += 50;
-                int scorePercentage = (countTotal - countAsked) * 100
-                        / countTotal;
-                if (scorePercentage > 90) {
-                    score = 100;
+        if (ResponsedAnswer.size() > 1) {
+            for (int i = 0; i < ResponsedAnswer.size(); i++) {
+                if (ResponsedAnswer.get(i).equals(CorrectAnswer)) {
+                    score += 50;
+                    int scorePercentage = (countTotal - countAsked) * 100
+                            / countTotal;
+                    if (scorePercentage > 90) {
+                        score = 100;
+                    } else {
+                        score += scorePercentage / 2;
+                    }
                 } else {
-                    score += scorePercentage / 2;
+                    score -= 10;
                 }
-            } else {
-                score -= 10;
+            }
+        } else if (ResponsedAnswer.size() == 1) {
+            for (int i = 0; i < countAsked; i++) {
+                if (AllSymptoms.contains(AskedSymptoms.get(i))) {
+                    score += 40 / countTotal;
+                    correctSymptoms++;
+                }
+                if (ResponsedAnswer.get(0).equals(CorrectAnswer)) {
+                    score += 30;
+                    score += (countTotal - correctSymptoms) * 30 / countTotal;
+                }
             }
         }
         return score;
+    }
+
+    @Override
+    public String MissSymptoms(ArrayList<String> AllSymptoms,
+            ArrayList<String> AskedSymptoms) {
+        String Message = "";
+        ArrayList<String> notAsked = AllSymptoms;
+        for (int i = 0; i < AllSymptoms.size(); i++) {
+            if (AllSymptoms.contains(AskedSymptoms.get(i))) {
+                notAsked.remove(AskedSymptoms.get(i));
+            }
+        }
+        for (int i = 0; i < notAsked.size(); i++) {
+            String Symptom = notAsked.get(i).substring(0, 1).toUpperCase()
+                    + notAsked.get(i).substring(1);
+            Symptom.replace("_", " ");
+            Message = "You missed the following symptoms: " + Symptom;
+            if(i!=notAsked.size()-1){
+                Message+=", ";
+            }else{
+                Message+=".";
+            }
+        }
+        return Message;
     }
 }
--- Android-App/chatbot-watson-android/app/src/main/java/com/example/vmac/WatBot/score/ScoreCalculator.java
+++ Android-App/chatbot-watson-android/app/src/main/java/com/example/vmac/WatBot/score/ScoreCalculator.java
@@ -5,4 +5,7 @@ public interface ScoreCalculator {
     public int getScore(ArrayList<String> AllSymptoms,
             ArrayList<String> AskedSymptoms, ArrayList<String> ResponsedAnswer,
             String CorrectAnswer);
+
+    public String MissSymptoms(ArrayList<String> AllSymptoms,
+            ArrayList<String> AskedSymptoms);
 }
